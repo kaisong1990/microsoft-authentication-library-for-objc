9c50d370d8ca0720256b485b4faebec3d440086f
kaisong1990 <kaisong1990@gmail.com>
2020-11-30 15:01:00 -0800

    updart url

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSAL",
-            url: "https://github.com/AzureAD/microsoft-authentication-library-for-objc/blob/kaisong1990/test-spm/MSAL.zip",
+            url: "https://github.com/kaisong1990/microsoft-authentication-library-for-objc/archive/MSAL.zip",
             checksum: "4dab34779871a8db991daa2883822cf2ae45a49b1f72d8010fbfec241f0cffde"
             )
     ]
